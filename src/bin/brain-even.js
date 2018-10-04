3e364651222d16cfba63beb657c23b3395c839d0
Mak <maxdmtrev@gmail.com>
2018-10-04 02:51:23 -0700

    add module brain-even

---
+++ src/bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import makeEvent from '../logic';
+
+makeEvent();
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ const userName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
 export default userName;
